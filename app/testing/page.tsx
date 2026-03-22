8d71e466b50398f9f9c3d96d25dae93c26e1b0d8
Juniper <basil.squared1@gmail.com>
2026-03-22 18:15:44 -0500

    work on textboxes

---
+++ app/testing/page.tsx
@@ -0,0 +1,7 @@
+import TestBox from "../ui/testTextBox"
+
+export default function Test() {
+    return (
+        <TestBox></TestBox>
+    )
+}
---
+++ app/ui/testTextBox.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react"
+
+export default function TestBox() {
+    const [text, setText] = useState("Hello, World!")
+    return (
+        <div>
+            <p className="font-bold text-sky-500">{text}</p>
+            <input value={text} onChange={evt => setText(evt.target.value)} />
+        </div>
+
+    )
+}
